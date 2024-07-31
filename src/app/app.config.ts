a98a714ce22f7328289ed8c06131f68fa3ef2b38
rgrueneis <robert.grueneis@penet.at>
2024-07-31 10:13:27 +0200

    First version

---
+++ src/app/app.config.ts
@@ -0,0 +1,21 @@
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+
+import { routes } from './app.routes';
+import { ApiModule, BASE_PATH } from './swagger';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(),
+    importProvidersFrom(ApiModule),
+    { provide: BASE_PATH, useValue: environment.apiRoot },
+    provideAnimations(),
+  ]
+};
+
+console.log('Based on Angular18 Template v18.0.1 [2024-06-27]');
---
+++ src/app/test-pages/test-general/test-general.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { OkStatus, ValuesService } from '../../swagger';
+import { NgSignalDirective } from '../../shared/ngSignal.directive';
+
+@Component({
+  selector: 'app-test-page',
+  standalone: true,
+  imports: [CommonModule, NgSignalDirective, MatButtonModule],
+  templateUrl: './test-general.component.html',
+  styleUrl: './test-general.component.scss'
+})
+export class TestPageComponent implements OnInit {
+  private valuesService = inject(ValuesService);
+  linqAverage = 0;
+  linqAverageExpected = 2.5;
+  myStr = signal('abc');
+  okStatus: OkStatus = { isOk: false, error: '', nr: -2 };
+  // okStatus: OkStatus = { isOk: true, error: '', nr: 1 };
+
+  ngOnInit(): void {
+    this.linqAverage = [1, 2, 3, 4].average(); //testing linq
+    this.valuesService.valuesSubjectsGet().subscribe(
+      {
+        next: x => this.okStatus = x,
+        error: err => this.okStatus.error = err.message,
+      });
+  }
+}
